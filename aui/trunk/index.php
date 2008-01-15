77b6995994c921d4d0f6ccb5ad3f820a5b3919a3
sergio <sergio@bafc8c6a-972d-4a8c-9a31-01add4bfe0c7>
2008-01-15 14:20:57 +0000

    Initial import: Ascia User Interface

---
+++ aui/trunk/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" lang="it_IT">
+<head>
+<title>Ascia User Interface</title>
+
+<link href="aui.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+
+<div title="AUI dispositivo di controllo senza fili"
+	style="margin: 0px auto; width: 481px; height: 615px;">
+
+
+<img src="images/AUI-ipod-top.jpg" width="481" height="107"/>
+<div style="float: left"><img src="images/AUI-ipod-sx.jpg"  width="97" height="320" /></div>
+
+<div title="schermo centrato verticale 240x320"
+	style="float: left; width: 240px; height: 320px; background-color: black; color: white;">
+<?php include('aui.php'); ?>	
+</div>
+
+<img src="images/AUI-ipod-dx.jpg"  width="144" height="320" />
+<img src="images/AUI-ipod-down.jpg" width="481" height="188"/>
+
+</div>
+
+</body>
+</html>

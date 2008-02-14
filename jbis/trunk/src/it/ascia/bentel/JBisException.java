63fba4b7118e514624ed10cd4f5535eb2fcdded9
Arrigo Marchiori <16649593+ardovm@users.noreply.github.com>
2008-02-14 18:56:24 +0000

    Interpretazione del messaggio di stato real-time della centralina. Metodi getter per le informazioni ricevute.

---
+++ jbis/trunk/src/it/ascia/bentel/JBisException.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright (C) 2008 ASCIA S.r.l.
+ */
+package it.ascia.bentel;
+
+/**
+ * Eccezione di JBis.
+ * @author arrigo
+ */
+public class JBisException extends Exception {
+	/**
+	 * Costruttore.
+	 * @param arg0 messaggio di errore
+	 */
+	public JBisException(String arg0) {
+		super(arg0);
+	}
+}

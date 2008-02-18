a407280b6079dc8bf8afc3f78d8b6b393721c393
Arrigo Marchiori <16649593+ardovm@users.noreply.github.com>
2008-02-18 18:16:49 +0000

    Gestione del log anti-doppioni che probabilmente e' sbagliata. :-( I dati ricevuti sono coerenti, ma non sembrano rispettare la doc della DLL. Da approfondire.

---
+++ jbis/trunk/src/it/ascia/bentel/Event.java
@@ -0,0 +1,110 @@
+/**
+ * Copyright (C) 2008 ASCIA S.r.l.
+ */
+package it.ascia.bentel;
+
+/**
+ * Un evento letto dai log.
+ * 
+ * @author arrigo
+ */
+public class Event {
+	/**
+	 * Lunghezza del timestamp in byte.
+	 */
+	public static final int TIMESTAMP_LENGTH = 5;
+	/**
+	 * Numero dell'evento.
+	 */
+	private int number;
+	/**
+	 * Timestamp, così come scritto nell'evento, ma riordinato.
+	 * 
+	 * <p>I byte sono, nell'ordine: anno - 2000, mese, giorno, ore, minuti.</p> 
+	 */
+	private byte[] timestamp;
+	
+	/**
+	 * Costruttore a partire dai dati di log.
+	 * 
+	 * @param logData log (dati grezzi) 
+	 * @param address indirizzo del primo byte dentro data che descrive l'evento
+	 */
+	public Event(byte []logData, int address) {
+		int tsAddress = address + 2;
+		number = (logData[address] << 8) + logData[address + 1];
+		timestamp = new byte[TIMESTAMP_LENGTH];
+		timestamp[0] = logData[tsAddress + 2]; // anno
+		timestamp[1] = logData[tsAddress + 1]; // mese
+		timestamp[2] = logData[tsAddress]; // giorno
+		timestamp[3] = logData[tsAddress + 3]; // ore
+		timestamp[4] = logData[tsAddress + 4]; // minuti
+	}
+	
+	/**
+	 * Ritorna il numero dell'evento.
+	 * 
+	 * @return il numero dell'evento.
+	 */
+	public int getNumber() {
+		return number;
+	}
+	
+	/**
+	 * Ritorna il timestamp, cosi' come specificato nel messaggio, 
+	 * ma riordinato.
+	 * 
+	 * <p>ATTENZIONE: ritorna la stessa array contenuta all'interno 
+	 * dell'oggetto!</p>
+	 * 
+	 * @return un'array di 5 byte. Il piu' significativo e' l'anno, il meno
+	 * sono i minuti.
+	 */
+	public byte[] getRawTimestamp() {
+		return timestamp;
+	}
+	
+	/**
+	 * Ritorna il timestamp sotto forma di stringa.
+	 */
+	public String getTimestamp() {
+		String retval;
+		retval = (timestamp[0] + 2000) + "-";
+		if (timestamp[1] < 10) retval += "0";
+		retval += timestamp[1] + "-";
+		if (timestamp[2] < 10) retval += "0";
+		retval += timestamp[2] + " ";
+		if (timestamp[3] < 10) retval += "0";
+		retval += timestamp[3] + ":";
+		if (timestamp[4] < 10) retval += "0";
+		retval += timestamp[4];
+		return retval;
+	}
+	
+	/**
+	 * Ritorna una descrizione dell'evento in forma testuale.
+	 */
+	public String getInfo() {
+		return getTimestamp() + "  " + getNumber();
+	}
+
+	/**
+	 * Confronta il timestamp di questo evento con quello di un altro.
+	 *
+	 * @param arg0 messaggio con cui fare il confronto.
+	 * 
+	 * @return 1 se questo messaggio e' piu' recente di arg0; 0 se ha lo stesso 
+	 * timestamp; -1 se questo messaggio e' piu' vecchio di arg0.
+	 */
+	public int compareTimestamp(Event arg0) {
+		byte otherTimestamp[] = arg0.getRawTimestamp();
+		for (int i = 0; i < TIMESTAMP_LENGTH; i++) {
+			if (timestamp[i] > otherTimestamp[i]) {
+				return 1;
+			} else if (timestamp[i] < otherTimestamp[i]) {
+				return -1;
+			}
+		}
+		return 0;
+	}
+}
--- jbis/trunk/src/it/ascia/bentel/JBisKyoUnit.java
+++ jbis/trunk/src/it/ascia/bentel/JBisKyoUnit.java
@@ -3,7 +3,6 @@
  */
 package it.ascia.bentel;
 
-import java.io.IOException;
 import java.util.Iterator;
 import java.util.LinkedList;
 
@@ -16,6 +15,16 @@ import java.util.LinkedList;
  * @author sergio, arrigo
  */
 public class JBisKyoUnit {
+	/**
+	 * Dimensione massima del registro interno alla centralina (n. di eventi).
+	 */
+	private static final int LOG_SIZE = 128;
+	
+	/**
+	 * Dimensione masssima del nostro log di eventi.
+	 */
+	private static final int MAX_LOG_SIZE = 100;
+	
 	/**
 	 * Array per tradurre i codici di errore numerici in stringhe.
 	 */
@@ -134,6 +143,24 @@ public class JBisKyoUnit {
 	 */
 	private byte otherSabotages;
 	
+	/**
+	 * Registro degli eventi. I piu' nuovi sono all'inizio.
+	 */
+	private LinkedList eventLog;
+	
+	/**
+	 * Uno degli ultimi eventi inseriti nella coda.
+	 * 
+	 * <p>Questo oggetto e' utile per capire rapidamente quali eventi del log
+	 * abbiamo gia' registrato: quelli piu' vecchi di questo.</p>
+	 */
+	private Event newEvent;
+	
+	/**
+	 * Numero di newEvent dentro al log.
+	 */
+	private int newEventIndex;
+	
 	
 	private String PIN;
 	
@@ -154,6 +181,8 @@ public class JBisKyoUnit {
 		seriale = (byte)s;
 		tentativi = (byte)t;
 		PIN = p;
+		eventLog = new LinkedList();
+		newEvent = null;
 		if (!openLibrary()) {
 			throw new JBisException("Impossibile aprire la DLL");
 		}
@@ -323,6 +352,88 @@ public class JBisKyoUnit {
 	}
 	
 	/**
+	 * Aggiorna il log degli eventi.
+	 * 
+	 * <p>Richiede il registro degli ultimi eventi alla centralina e aggiorna
+	 * il registro interno di questa classe.</p>
+	 * 
+	 * <p>Gli eventi sono inseriti nel log solo se non sono piu' vecchi di 
+	 * firstEvent.</p>
+	 * 
+	 * @throws JBisException in caso di errore di accesso alla centralina.
+	 */
+	public void updateLog() throws JBisException {
+		byte[] data = Leggi(0x30d);
+		int eventsNumber, firstEvent, event, eventAddress;
+		// data[0] se 0 indica che il buffer ha circolato.
+		// data[1] e' il numero della prossima entry che sara' inserita.
+		if (data[0] == 0) { // Il buffer circolare ha circolato
+			System.out.println("Il buffer ha circolato!");
+			eventsNumber = LOG_SIZE;
+			firstEvent = data[1] & 0xff;
+		} else {
+			eventsNumber = data[1] & 0xff;
+			firstEvent = 0;
+		}
+		System.out.println("Eventi da leggere: " + eventsNumber);
+		System.out.println("Primo evento:" + firstEvent);
+		for (event = 0; event < eventsNumber; event++) {
+			boolean isNew = false, isNewest = false;
+			System.out.println("Evento n. " + event);
+			// L'indirizzo deve circolare con il buffer
+			if ((event + firstEvent) < LOG_SIZE) {
+				eventAddress = 2 + (event + firstEvent) * 7;
+			} else {
+				eventAddress = 2 + (event + firstEvent - LOG_SIZE) * 7;
+				System.out.println("Rolling");
+			}
+			System.out.println("eventAddress: " + eventAddress);
+			Event ev = new Event(data, eventAddress);
+			if (newEvent != null) {
+				int comparation = newEvent.compareTimestamp(ev);
+				if (comparation < 0) {
+					// Questo evento e' piu' nuovo dell'ultimo nel log.
+					isNew = true;
+					isNewest = true;
+				} else if (comparation == 0) {
+					// Questo evento ha lo stesso timestamp dell'ultimo/i.
+					// Deve essere confrontato con esso e con gli altri che
+					// hanno lo stesso timestamp.
+					isNew = true;
+					for (int i = newEventIndex; 
+						(i >= 0) && isNew; 
+						i--) {
+						Event logged = (Event)eventLog.get(i);
+						if (ev.getNumber() == logged.getNumber()) {
+							isNew = false;
+						}
+					}
+				}
+			} else { // Il log e' vuoto.
+				isNew = true;
+				isNewest = true;
+			}
+			if (isNew) {
+				eventLog.addFirst(ev);
+				System.out.println(ev.getInfo());
+				if (isNewest) {
+					// E' il piu' nuovo del log. Sara' il nostro prossimo metro. 
+					newEvent = ev;
+					newEventIndex = 0;
+				} else {
+					// Abbiamo scalato in giu' il primo evento con il timestamp
+					// piu' nuovo.
+					newEventIndex++;
+				}
+			} // if isNew
+		} // Cicla su tutti gli eventi
+		// Eliminazione dei messaggi in eccesso
+		while (eventLog.size() > MAX_LOG_SIZE) {
+			eventLog.removeLast();
+		}
+	}
+	
+	/**
 	 * Converte un codice di errore numerico in un messaggio testuale.
 	 * 
 	 * @param err codice di errore.
@@ -343,7 +454,7 @@ public class JBisKyoUnit {
     	JBisKyoUnit b;
 		try {
 			b = new JBisKyoUnit(1,1,"0025");
-			b.updateStatus();
+			/*b.updateStatus();
 			if (b.hasAlarms()) {
 				System.out.println("Allarmi:");
 				if (b.hasZoneAlarms()) {
@@ -389,7 +500,10 @@ public class JBisKyoUnit {
 				while (it.hasNext()) {
 					System.out.println(" " + it.next());
 				}
-			} // Warning
+			} // Warning */
+			System.out.println();
+			System.out.println("Lettura log...");
+			b.updateLog();
 			b.close();
 		} catch (JBisException e) {
 			System.err.println(e.getMessage());
@@ -400,11 +514,11 @@ public class JBisKyoUnit {
     byte[] Leggi(int comando) throws JBisException {
 		Integer[] res1 = new Integer[1];
 		res1[0] = Integer.valueOf(comando);
-		System.out.printf("Comando: 0x%h\r\n",res1);
+		// System.out.printf("Comando: 0x%h\r\n",res1);
     	byte[] data;
-    	data = new byte[512];
+    	data = new byte[1024];
     	byte result = PanelConnection(comando, seriale, tentativi, PIN, data);
-    	System.out.println("result: " + result);
+    	// System.out.println("result: " + result);
     	if (result != 0) {
     		Byte[] res = new Byte[1];
     		res[0] = Byte.valueOf(result);
@@ -421,27 +535,28 @@ public class JBisKyoUnit {
     }
 
     /**
+     * Comunicazione con la centralina attraverso la seriale.
      * 
-     * 0: Operazione andata a buon fine
-     * 1: Codice utente non valido
-     * 2: Errore apertura porta seriale
-     * 3: Errore di comunicazione
-     * 4: Comando sconosciuto
-     * 5: Tipo centrale non riconosciuto
-     * 6: Versione firmware non riconosciuto
-     * 7: Aree inserite, impossibile aprire sessione di programmazione
-     * 8: Sessione di programmazione già aperta
-     * 9: Dati forniti per la scrittura non validi
-     * 10: Errore chiusura sessione di programmazione
-     * 11: Codice utente non abilitato
+     * <p>0: Operazione andata a buon fine<br>
+     * 1: Codice utente non valido<br>
+     * 2: Errore apertura porta seriale<br>
+     * 3: Errore di comunicazione<br>
+     * 4: Comando sconosciuto<br>
+     * 5: Tipo centrale non riconosciuto<br>
+     * 6: Versione firmware non riconosciuto<br>
+     * 7: Aree inserite, impossibile aprire sessione di programmazione<br>
+     * 8: Sessione di programmazione già aperta<br>
+     * 9: Dati forniti per la scrittura non validi<br>
+     * 10: Errore chiusura sessione di programmazione<br>
+     * 11: Codice utente non abilitato</p>
      * 
-     * @param comando
-     * @param seriale
-     * @param tentativi
-     * @param PIN
-     * @param PINLength
-     * @param data
-     * @return
+     * @param comando numero del comando da inviare
+     * @param seriale numero della porta seriale
+     * @param tentativi numero di tentativi da effettuare in caso di errore
+     * @param PIN codice PIN
+     * @param PINLength lunghezza della stringa che contiene il PIN
+     * @param data dati da inviare
+     * @return vedi sopra.
      */
     private static native byte PanelConnection(int comando, byte seriale, byte tentativi, String PIN, byte[] data);
 

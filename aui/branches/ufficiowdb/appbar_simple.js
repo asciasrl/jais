c91658cc41eb570116902fcec271d3f8925c800d
arrigo <arrigo@bafc8c6a-972d-4a8c-9a31-01add4bfe0c7>
2008-02-28 10:30:35 +0000

    Branch ufficiowdb

---
+++ aui/branches/ufficiowdb/appbar_simple.js
@@ -0,0 +1,75 @@
+/**
+ * appbar_simple.js
+ * (C) 2008 Ascia S.r.l.
+ */
+
+/**
+ * Numero dell'icona attualmente centrata (selezionata).
+ */
+var selectedAppbarIconNumber;
+
+/**
+ * Mostra la barra delle applicazioni.
+ *
+ * <p>Questa funzione legge selectedAppBarIconNumber.</p>
+ */
+function appbar_show() {
+	// determino per ogni icona l'opacita'
+	for (i=1; i <= appbar_num; i++) {
+		var size, opacity;
+		if (i == selectedAppbarIconNumber + 1) {
+			opacity = 100;
+			size = 80;
+			statusMessage("app-" + i + ", " + size + ", " + opacity);
+		} else {
+			opacity = 50;
+			size = 65;
+		}
+		var div_obj = appbar_icoset('app-'+i,size,opacity, true);
+		// Le icone piccole devono essere piu' distanziate da quelle vicine.
+		var newMargin = ((80-size)/2)+'px';
+		div_obj.style.marginLeft = newMargin;
+		div_obj.style.marginRight = newMargin;
+	}
+}
+
+//appbar_show(300);
+//appbar_show(320);
+
+
+/**
+ * Attiva il servizio rappresentato dall'icona piu' vicina alla posizione in
+ * cui e' arrivato il click del mouse.
+ */
+function selectClickedIcon(x) {
+	selectedAppbarIconNumber = Math.round((x - 40) / 80);
+	var selectedService = SERVICES[selectedAppbarIconNumber];
+	iconSelected(selectedService);
+	appbar_show();
+	//statusMessage("x: " + x + "  -> " + selectedService);
+}
+
+/**
+ * Gestisce un trascinamento sull'appbar.
+ *
+ * @param mousePos posizione attuale del mouse relativa all'appbar.
+ */
+function dragAppBar(mousePos) {
+	selectClickedIcon(mousePos.x); 
+}
+
+/**
+ * Risponde a un click.
+ *
+ * <p>Questa funzione viene chiamata quando il bottone del mouse viene 
+ * rilasciato.</p>
+ *
+ * @param mousePos posizione del mouse relativa all'appbar.
+ * @param timeStamp istante in cui il bottone viene rilasciato.
+ */
+function dragAppBarStop(mousePos, timeStamp) {
+	selectClickedIcon(mousePos.x);
+}
+
+selectClickedIcon(APPBAR_START_POSITION);
+makeDraggable(document.getElementById('appbar'));
---
+++ aui/branches/ufficiowdb/custom/config.php
@@ -0,0 +1,163 @@
+<?php
+/**
+ * Copyright (C) 2008 ASCIA S.r.l.
+ *
+ * <p>Questo file contiene le informazioni di configurazione di AUI
+ * personalizzate per un cliente.</p>
+ */
+
+/**
+ * Posizione iniziale della appBar [pixel].
+ *
+ * <p>Se la appBar non scorre, questo numero deve indicare la posizione della
+ * icona da selezionare all'avvio di AUI.</p>
+ */
+define(APPBAR_START_POSITION, 360);
+
+/**
+ * True se la appbar non deve fare lo scrolling.
+ */
+define("APPBAR_SIMPLE", false);
+
+/**
+ * Lista dei servizi.
+ */
+$apps = array('audio','clima','energia','illuminazione','serramenti','sicurezza','video');
+
+/**
+ * Altezza della "parte utile" dello slider del dimmer [pixel].
+ *
+ * <p>Questa deve essere l'altezza dentro la quale si puo' spostare il cursore
+ * del dimmer.</p>
+ */
+define("DIMMER_SLIDER_HEIGHT", 100);
+
+/**
+ * Immagine che mostra i piani.
+ */
+$pianiFile = "custom/images/assonometria320x370.png"; // images/piani-all.png";
+$pianiSize = Array("w" => IPOD_VIEWPORT_WIDTH, "h" => 370); // Array("w" => 240, "h" => 240);
+
+/**
+ * Lista dei piani.
+ */
+$piani = Array(
+	Array(
+		"id" => "piano-01A",
+		"header" => "Piano 1A",
+		"mapFile" => "custom/images/planimetria.png",
+		"mapSize" => Array("w" => IPOD_VIEWPORT_WIDTH, "h" => 299),
+		"bigMapFile" => "custom/images/planimetria-big.png",
+		"bigMapSize" => Array("w" => 800, "h" => 748)));
+
+/* Vecchi piani
+$piani = Array(
+	Array(
+		"id" => "piano-01A",
+		"header" => "Piano 1A",
+		"mapFile" => "images/piano-01A.png",
+		"bigMapFile" => "images/piano-01A-big.png",
+		"bigMapSize" => Array("w" => 720, "h" => 720)));
+*/
+
+	define("ILL_LUCE", 0);
+	define("ILL_DIMMER", 1);
+
+/**
+ * Luci presenti nel sistema.
+ * 
+ * <p>Questa array di ID verra' popolata in fase di creazione delle luci.</p>
+ */
+$idLuci = Array();
+
+/**
+ * Frame: illuminazione.
+ * 
+ * <p>Gli indici sono gli ID dei piani.</p>
+ */
+$frameIlluminazione = Array(
+	"piano-01A" => Array(
+		"p1a-luce1" => Array(
+			"type" => ILL_LUCE,
+			"x" => 100,
+			"y" => 100,
+			"label" => "Applique",
+			"address" => "0.3:Out1"),
+		"p1a-luce2" => Array(
+			"type" => ILL_LUCE,
+			"x" => 300,
+			"y" => 100,
+			"label" => "Luce pitosforo",
+			"address" => "0.3:Out2"),
+		"p1a-dimmer1" => Array(
+			"type" => ILL_DIMMER,
+			"x" => 100,
+			"y" => 340,
+			"label" => "Dimmer allarme",
+			"address" => "0.5:Out1"),
+		"p1a-dimmer2" => Array(
+			"type" => ILL_DIMMER,
+			"x" => 300,
+			"y" => 340,
+			"label" => "Dimmer BMC virtuale",
+			"address" => "0.5:Out2")));
+
+/**
+ * Prese comandate presenti nel sistema.
+ * 
+ * <p>Questa array di ID verra' popolata in fase di creazione delle prese
+ * comandate.</p>
+ */
+$idPrese = Array();
+
+/**
+ * Frame: energia.
+ * 
+ * <p>Gli indici sono gli ID dei piani.</p>
+ */
+$frameEnergia = Array(
+	"piano-01A" => Array(
+		"p1a-presa1" => Array(
+			"x" => 200,
+			"y" => 200,
+			"label" => "Presa 1",
+			"address" => "0.3:Out3"),
+		"p1a-presa2" => Array(
+			"x" => 400,
+			"y" => 200,
+			"label" => "Presa pitosforo",
+			"address" => "0.3:Out4"),
+		"p1a-presa3" => Array(
+			"x" => 200,
+			"y" => 400,
+			"label" => "Presa lavatrice",
+			"address" => "0.5:Out5")));
+
+/**
+ * Termostati presenti nel sistema.
+ * 
+ * <p>Questa array di ID verra' popolata in fase di creazione delle prese
+ * comandate.</p>
+ */
+$idClimi = Array();
+
+/**
+ * Frame: clima.
+ * 
+ * <p>Gli indici sono gli ID dei piani.</p>
+ */
+$frameClima = Array(
+	"piano-01A" => Array(
+		"p1a-clima1" => Array(
+			"x" => 300,
+			"y" => 200,
+			"label" => "Termostato a vapore",
+			"address" => "0.3:Out6"),
+		"p1a-clima2" => Array(
+			"x" => 400,
+			"y" => 400,
+			"label" => "Bruciatore",
+			"address" => "0.3:Out7")));
+
+
+?>

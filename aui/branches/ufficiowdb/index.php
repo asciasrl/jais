3c88e25fb5ef513947a848d9ad598372daf3bdfd
Arrigo Marchiori <16649593+ardovm@users.noreply.github.com>
2008-02-28 12:11:47 +0000

    Merge dell'ultimo commit su trunk: appbar che sparisce se non serve.

--- aui/branches/ufficiowdb/index.php
+++ aui/branches/ufficiowdb/index.php
@@ -7,6 +7,7 @@ require_once("custom/config.php");
  */
 $mobile = !((strpos($_SERVER['HTTP_USER_AGENT'], "iPod") === false) &&
 	(strpos($_SERVER['HTTP_USER_AGENT'], "iPhone") === false));
+// $mobile = true;
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 	   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">

80998924ced494c8850e11f91ba2547974b97f28
arrigo <arrigo@bafc8c6a-972d-4a8c-9a31-01add4bfe0c7>
2008-05-23 16:58:39 +0000

    Piu' mini del mini... micro!

---
+++ aui/branches/micro/config.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * Copyright (C) 2008 ASCIA S.r.l.
+ *
+ * <p>Questo file contiene le informazioni di configurazione di default per 
+ * AUI.</p>
+ *
+ * <p>Queste informazioni devono essere integrate (eventualmente anche
+ * sovrascritte) dal file custom/config.php.</p>
+ */
+
+/**
+ * Larghezza della "viewport" dell'iPod Touch. [pixel]
+ */
+define("IPOD_VIEWPORT_WIDTH", 320);
+
+/**
+ * Altezza della "viewport" dell'iPod Touch. [pixel]
+ */
+define("IPOD_VIEWPORT_HEIGHT", 356);
+
+/**
+ * Altezza dell'area in cui si visualizza la mappa. [pixel]
+ */
+define("IPOD_MAP_AREA_HEIGHT", 276); 
+
+/**
+ * Altezza della "parte stondata" degli estremi dello slider del dimmer [pixel].
+ *
+ * <p>Gli estremi del dimmer hanno gli angoli arrotondati. Questa e' l'altezza
+ * di tali angoli, che stanno disegnati dentro l'immagine degli estremi dello
+ * slider.</p>
+ */
+define("DIMMER_SLIDER_CORNER_HEIGHT", 5);
+
+/**
+ * Larghezza del bordo dello slider del dimmer.
+ *
+ * <p>Lo slider del dimmer ha un bordo che deve essere simmetrico rispetto
+ * all'asse verticale. La sua larghezza in pixel e' questa.</p>
+ */
+define("DIMMER_SLIDER_BORDER_WIDTH", 6);
+
+/**
+ * Immagine che contiene la parte superiore dello slider del dimmer.
+ */
+define("IMG_DIMMER_SLIDER_TOP", "images/dimmer-top.png");
+
+/**
+ * Immagine che contiene la parte inferiore dello slider del dimmer.
+ */
+define("IMG_DIMMER_SLIDER_BOTTOM", "images/dimmer-bottom.png");
+
+/**
+ * Immagine che contiene un segmento della parte centrale dello slider del 
+ * dimmer.
+ *
+ * <p>Lo "sfondo" dello slider del dimmer sara' questa immagine ripetuta.</p>
+ */
+define("IMG_DIMMER_SLIDER_MIDDLE", "images/dimmer-sfondo.png");
+
+/**
+ * Cursore del dimmer.
+ */
+define("IMG_DIMMER_CURSOR", "images/dimmer-tasto.png");
+
+/**
+ * Controllo delle tapparelle.
+ */
+define("IMG_BLIND_CONTROL", "images/serrande.png");
+
+/**
+ * Coordinata y del bordo superiore del bottone "up".
+ */
+define("BLIND_UP_TOP", 12);
+
+/**
+ * Coordinata x del bordo sinistro dei bottoni "up".
+ */
+define("BLIND_LEFT", 6);
+
+/**
+ * Coordinata y del bordo inferiore del bottone "up".
+ */
+define("BLIND_UP_BOTTOM", 42);
+
+/**
+ * Coordinata x del bordo destro dei bottoni.
+ */
+define("BLIND_RIGHT", 34);
+
+/**
+ * Coordinata y del bordo inferiore del bottone "down".
+ */
+define("BLIND_DOWN_TOP", 48);
+
+/**
+ * Coordinata y del bordo inferiore del bottone "down".
+ */
+define("BLIND_DOWN_BOTTOM", 78);
+
+// ---------------------------------------------------------
+// Costanti per uso interno
+
+define("ILL_LUCE", 0);
+define("ILL_DIMMER", 1);
+define("SIC_LUCCHETTO", 0);
+define("SIC_PORTA", 1);
+
+
+/**
+ * Icone varie.
+ */
+define("IMG_LIGHT_ON", "images/luce_on.png");
+define("IMG_LIGHT_OFF", "images/luce_off.png");
+define("IMG_POWER_ON", "images/energia_on.png");
+define("IMG_POWER_OFF", "images/energia_off.png");
+define("IMG_THERMO_ON", "images/clima_on.png");
+define("IMG_THERMO_OFF", "images/clima_off.png");
+define("IMG_BLIND_STILL", "images/serranda_ferma.png");
+define("IMG_BLIND_OPENING", "images/serranda_sale.png");
+define("IMG_BLIND_CLOSING", "images/serranda_scende.png");
+define("IMG_LOCK_OPEN", "images/allarm_off.png");
+define("IMG_LOCK_CLOSE", "images/allarm_on.png");
+define("IMG_DOOR_OPEN", "images/porta_aperta.png");
+define("IMG_DOOR_CLOSE", "images/porta_chiusa.png");
+define("IMG_DOOR_OPEN_ALARM", "images/porta_aperta_red.png");
+define("IMG_DOOR_CLOSE_OK", "images/porta_chiusa_green.png");
+define("IMG_SCENARIOS", "images/scenari_all.png");
+
+/**
+ * Altezza della status bar [pixel].
+ */
+define("STATUS_BAR_HEIGHT", 40);
+
+/**
+ * Opacita' di default della status bar [0 .. 1].
+ */
+define("STATUS_BAR_OPACITY", 0.60);
+
+
+?>
---
+++ aui/branches/micro/statusbar.js
@@ -0,0 +1,73 @@
+/**
+ * Copyright (C) 2008 ASCIA S.R.L.
+ */
+
+/**
+ * Per quanto tempo la barra di stato viene visualizzata. [msec]
+ */
+const STATUS_BAR_TIMEOUT = 3000;
+/**
+ * Velocita' di sparizione della status bar. [percentuale di opacita'/sec]
+ */
+const STATUS_BAR_DISAPPEARING_SPEED = 30;
+/**
+ * Periodo del timer che fa sparire la status bar [msec].
+ */
+const STATUS_BAR_DISAPPEARING_PERIOD = 100;
+/**
+ * Div che contiene la barra di stato, lo sfondo ecc.
+ */
+var statusBarContainer = document.getElementById("header-out");
+/**
+ * Stato attuale di sparizione della barra.
+ */
+var currentStatusBarStatus = 0;
+/**
+ * Conto alla rovescia per far sparire la barra di stato.
+ *
+ * <p>Vale false se la barra e' nascosta completamente.</p>
+ */
+var statusBarTimeout = false;
+/**
+ * &lt;div&gt; che contiene la status bar.
+ */
+var statusObject = document.getElementById("header");
+
+
+/**
+ * Fa apparire la barra di stato e inizia il conto alla rovescia per farla
+ * sparire.
+ *
+ * @param message il messaggio da visualizzare.
+ */
+function statusMessage(message) {
+	statusObject.innerHTML = message;
+	statusBarContainer.style.display = "";
+	if (statusBarTimeout) {
+		window.clearTimeout(statusBarTimeout);
+		statusBarContainer.style.opacity = STATUS_BAR_OPACITY;
+	}
+	statusBarTimeout = window.setTimeout("closeStatusBar()", 
+		STATUS_BAR_TIMEOUT);
+	currentStatusBarStatus = STATUS_BAR_OPACITY;
+}
+
+/**
+ * Fa sparire lentamente la barra di stato.
+ * TODO
+ */
+function closeStatusBar() {
+	currentStatusBarStatus -= STATUS_BAR_DISAPPEARING_SPEED * 
+		STATUS_BAR_DISAPPEARING_PERIOD / 100000;
+	if (currentStatusBarStatus <= 0) {
+		// Siamo arrivati in fondo.
+		statusBarContainer.style.display = "none";
+		statusBarContainer.style.opacity = STATUS_BAR_OPACITY;
+		currentStatusBarStatus = STATUS_BAR_OPACITY;
+		statusBarTimeout = false;
+	} else {
+		statusBarContainer.style.opacity = currentStatusBarStatus;
+		statusBarTimeout = window.setTimeout("closeStatusBar()", 
+			STATUS_BAR_DISAPPEARING_PERIOD);
+	}
+}

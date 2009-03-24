1ce9fb8176afb186ce452a7b17e9573aa5cb2e85
Sergio Strampelli <sergio.strampelli@gmail.com>
2009-03-24 20:12:24 +0000

    spostate b2h e b2b da EDSMessage a Message

--- jais/trunk/src/it/ascia/ais/Message.java
+++ jais/trunk/src/it/ascia/ais/Message.java
@@ -2,7 +2,9 @@ package it.ascia.ais;
 
 public abstract class Message {
 
-	public abstract String getMessageDescription();
+	public String getMessageDescription() {
+		return getClass().getSimpleName();
+	}
 	
 	public abstract String toString();
 	
@@ -14,7 +16,41 @@ public abstract class Message {
 	 *  - getValue
 	 */
 	
+	/**
+	 * Ritorna il contenuto del messaggio sotto forma di array di byte.
+	 */
+	public abstract byte[] getBytesMessage();
+	
 	public abstract String getSource();
 	
 	public abstract String getDestination();
+	
+	/**
+	 * Funzione di utilita': restituisce la rappresentazione esadecimale
+	 * di un byte con 2 cifre e preceduta da "0x"
+	 */
+	public static String b2h(int i)
+	{
+		String s = "0x";
+		if (i < 16) {
+			s += "0";
+		}
+		s += Integer.toHexString(i);
+		return s;
+	}
+
+	/**
+	 * Funzione di utilita': restituisce la rappresentazione binaria
+	 * di un byte con esattamente 8 bit
+	 */
+	public static String b2b(int i)
+	{
+		StringBuffer s = new StringBuffer();
+		for (int j = 0; j < 8; j++) {
+			s.append((i >> j) & 1);
+		}
+		return s.reverse().toString();
+	}
+
+
 }

3f3aabfcb5407fb34c5850684708060b6fcb1b70
sergio <sergio@bafc8c6a-972d-4a8c-9a31-01add4bfe0c7>
2009-03-24 12:34:18 +0000

    BMCRegT22 funzionante ...

--- jeds/trunk/src/it/ascia/eds/device/BMCRegT22.java
+++ jeds/trunk/src/it/ascia/eds/device/BMCRegT22.java
@@ -5,17 +5,17 @@ package it.ascia.eds.device;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
-
 import it.ascia.ais.AISException;
 import it.ascia.ais.Connector;
+import it.ascia.ais.DevicePort;
 import it.ascia.eds.EDSConnector;
 import it.ascia.eds.msg.ImpostaParametroMessage;
 import it.ascia.eds.msg.EDSMessage;
+import it.ascia.eds.msg.ImpostaSetPointMessage;
 import it.ascia.eds.msg.RichiestaAssociazioneUscitaMessage;
 import it.ascia.eds.msg.RichiestaParametroMessage;
 import it.ascia.eds.msg.RichiestaSetPointMessage;
 import it.ascia.eds.msg.RichiestaStatoTermostatoMessage;
-import it.ascia.eds.msg.RispostaAssociazioneUscitaMessage;
 import it.ascia.eds.msg.RispostaParametroMessage;
 import it.ascia.eds.msg.RispostaSetPointMessage;
 import it.ascia.eds.msg.RispostaStatoTermostatoMessage;
@@ -69,6 +69,21 @@ public class BMCRegT22 extends BMCStandardIO {
 		addPort(port_alarmTemp);
 		addPort(port_autoSendTime);
 		addPort(port_setPoint);
+		for (int stagione = 0; stagione <= 1; stagione++) {
+			for (int giorno = 0; giorno <= 6; giorno++) {
+				for (int ora = 0; ora <= 23; ora++) {
+					addPort(getSetPointPortId(stagione,giorno,ora));					
+				}
+			}		
+		}
+	}
+	
+	public static String getSetPointPortId(int stagione, int giorno, int ora) {
+		if (ora == 31) {
+			return "setPoint";
+		} else {
+			return "setPoint-"+ImpostaSetPointMessage.stagione(stagione)+"-"+ImpostaSetPointMessage.giorno(giorno)+"-"+ImpostaSetPointMessage.ora(ora);
+		}
 	}
 	
 	/* (non-Javadoc)
@@ -94,9 +109,14 @@ public class BMCRegT22 extends BMCStandardIO {
 	 * @see it.ascia.eds.device.BMC#getInfo()
 	 */
 	public String getInfo() {
-		return getName() + "BMC regolatore di temperatura (modello " + model + ")";
+		return getName() + " Cronotermostato WDB (modello " + model + ")";
 	}
 
+	public void discover() {
+		discoverBroadcastBindings();
+		readSetPoints();
+	}
+	
 	public void discoverUscite() {
 		// RegT non ha uscite configurabili
 	}
@@ -138,6 +158,8 @@ public class BMCRegT22 extends BMCStandardIO {
 		case EDSMessage.MSG_CAMBIO_VELOCITA:
 		case EDSMessage.MSG_PROGRAMMAZIONE:
 		case EDSMessage.MSG_RICHIESTA_PARAMETRO:
+		case EDSMessage.MSG_RICHIESTA_STATO:
+		case EDSMessage.MSG_IMPOSTA_SET_POINT:
 		case EDSMessage.MSG_RICHIESTA_STATO_TERMOSTATO:
 		case EDSMessage.MSG_RICHIESTA_SET_POINT:
 			// messaggi gestiti dal BMC reale
@@ -164,8 +186,23 @@ public class BMCRegT22 extends BMCStandardIO {
 			}
 			break;
 		case EDSMessage.MSG_RISPOSTA_SET_POINT:
-			RispostaSetPointMessage rspm = (RispostaSetPointMessage) m;
-			setPortValue(port_setPoint,new Double(rspm.getSetPoint()));
+			RispostaSetPointMessage risp = (RispostaSetPointMessage) m;
+			RichiestaSetPointMessage rich = (RichiestaSetPointMessage) risp.getRequest();
+			int stagione = risp.getStagione();
+			if (rich.getStagione() != stagione) {
+				logger.error("Risposta 'stagione' non coerente. "+rich+" "+risp);
+			}
+			int giorno = risp.getGiorno();
+			if (rich.getGiorno() != giorno) {
+				logger.error("Risposta 'giorno' non coerente");
+			}
+			int ora = risp.getOra();
+			if (rich.getOra() != ora) {
+				logger.error("Risposta 'ora' non coerente");
+			}
+			DevicePort p = getPort(getSetPointPortId(stagione,giorno,ora));
+			p.setCacheRetention(86400000);	// 1 giorno		
+			p.setValue(new Double(risp.getSetPoint()));
 			break;
 		case EDSMessage.MSG_RISPOSTA_STATO_TERMOSTATO:
 			RispostaStatoTermostatoMessage rsttm = (RispostaStatoTermostatoMessage) m;
@@ -217,7 +254,7 @@ public class BMCRegT22 extends BMCStandardIO {
 	 * <p>Invia un messaggio al BMC richiedendo il valore del parametro.</p>
 	 */
 	public long updateSetPoint() {
-		getConnector().sendMessage(new RichiestaSetPointMessage(getIntAddress(), getBMCComputerAddress()));
+		getConnector().sendMessage(new RichiestaSetPointMessage(getIntAddress(), getBMCComputerAddress(), true));
 		// FIXME calcolare il timeout
 		return 300;
 	}
@@ -233,24 +270,12 @@ public class BMCRegT22 extends BMCStandardIO {
 	
 	public long updatePort(String portId) {
 		if (portId.startsWith("Inp")) {
-			try {
-				setPortValue(portId, new Boolean(false));
-			} catch (AISException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			//FIXME Gestire aggiornamento stato, se applicabile
-			return 0;
-		} else if (portId.startsWith("Out")) {
-			// le porte "Out" sono "finte", quindi non possono essere aggiornate 
-			try {
-				setPortValue(portId, new Boolean(false));
-			} catch (AISException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
 			return super.updateStatus();
-			//return 0;
+		} else if (portId.startsWith("Out")) {
+			return super.updateStatus();
+		} else if (portId.startsWith("setPoint-")) {
+			readSetPoint(portId);
+			return 300;
 		} else {
 			return updateStatus();
 		}
@@ -271,24 +296,61 @@ public class BMCRegT22 extends BMCStandardIO {
 	 
 	
 	public boolean writePort(String portId, Object newValue) throws AISException {
+		boolean res = false;
 		if (portId.equals(port_autoSendTime)) {
-			return getConnector().sendMessage(new ImpostaParametroMessage(getIntAddress(), getBMCComputerAddress(), 
+			res = getConnector().sendMessage(new ImpostaParametroMessage(getIntAddress(), getBMCComputerAddress(), 
 					((Integer) newValue).intValue(),
-					ImpostaParametroMessage.PARM_TEMPERATURE));
+					ImpostaParametroMessage.PARM_TEMPERATURE)); 
 		} else
 		if (portId.equals(port_alarmTemp)) {
-			return getConnector().sendMessage(new ImpostaParametroMessage(getIntAddress(), getBMCComputerAddress(), 
+			res = getConnector().sendMessage(new ImpostaParametroMessage(getIntAddress(), getBMCComputerAddress(), 
 					((Integer) newValue).intValue(),					
 					ImpostaParametroMessage.PARAM_TERM_ALARM_TEMPERATURE));
 		} else 
 		if (portId.equals(port_autoSendTime)) {
-			return getConnector().sendMessage(new ImpostaParametroMessage(getIntAddress(), getBMCComputerAddress(), 
-				ImpostaParametroMessage.PARM_TIME,
-				((Integer) newValue).intValue()));
+			res = getConnector().sendMessage(new ImpostaParametroMessage(getIntAddress(), getBMCComputerAddress(), 
+				ImpostaParametroMessage.PARM_TIME, ((Integer) newValue).intValue()));
+		} else if (portId.startsWith("setPoint-")) {
+			String[] temp = portId.split("-");
+			String stagione = temp[1];
+			String giorno = temp[2];
+			int ora = Integer.parseInt(temp[3]);
+			//logger.info("write:"+stagione+","+giorno+","+ora+"="+newValue);
+			res = getConnector().sendMessage(new ImpostaSetPointMessage(getIntAddress(), getBMCComputerAddress(),
+					(Double)newValue, stagione, giorno, ora));
 		} else {
 			logger.fatal("Non so come scrivere sulla porta "+portId);
-			return false;
+		}
+		if (res) {
+			setPortValue(portId, newValue);
+		}
+		return res;
+	}
+	
+	public void readSetPoints() {
+		EDSConnector conn = (EDSConnector) getConnector();
+		int m = conn.getMyAddress();
+		int d = getIntAddress();
+		for (int stagione = 0; stagione <= 1; stagione++) {
+			for (int giorno = 0; giorno <= 6; giorno++) {
+				for (int ora = 0; ora <= 23; ora++) {					
+					RichiestaSetPointMessage rich = new RichiestaSetPointMessage(d,m,stagione,giorno,ora);
+					conn.queueMessage(rich);
+				}
+			}		
 		}
 	}
+	
+	public void readSetPoint(String portId) {
+		EDSConnector conn = (EDSConnector) getConnector();
+		int m = conn.getMyAddress();
+		int d = getIntAddress();
+		String[] temp = portId.split("-");
+		String stagione = temp[1];
+		String giorno = temp[2];
+		int ora = Integer.parseInt(temp[3]);
+		RichiestaSetPointMessage rich = new RichiestaSetPointMessage(d,m,stagione,giorno,ora);
+		conn.queueMessage(rich);
+	}	
 
 }

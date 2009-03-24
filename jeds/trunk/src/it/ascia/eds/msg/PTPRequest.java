7fedc2cabcc18e848acc00307dff3c633e155377
sergio <sergio@bafc8c6a-972d-4a8c-9a31-01add4bfe0c7>
2009-03-24 12:27:43 +0000

    spostato isAnsweredBy da PTPRequest a PTPMessage

--- jeds/trunk/src/it/ascia/eds/msg/PTPRequest.java
+++ jeds/trunk/src/it/ascia/eds/msg/PTPRequest.java
@@ -15,29 +15,6 @@ public abstract class PTPRequest extends PTPMessage {
 	private boolean isAnswered = false;
 	
 	/**
-	 * Verifica se un messaggio risponde a questo.
-	 * 
-	 * Il risultato viene deve essere ritornato e impostato nell'attributo
-	 * answered, che viene letto da wasAnswered().
-	 * 
-	 * Questo metodo riconosce risposte di tipo "Acknowledge". Se le sottoclassi
-	 * richiedono messaggi di risposta diversi, devono riscrivere questo metodo.
-	 * 
-	 * @return true se m e' la risposta a questo messaggio.
-	 */
-	public boolean isAnsweredBy(PTPMessage m) {
-		if (m.getMessageType() == EDSMessage.MSG_ACKNOWLEDGE) {
-			AcknowledgeMessage ack = (AcknowledgeMessage) m;
-			if ((getSender() == ack.getRecipient()) &&
-					(getRecipient() == ack.getSender()) &&
-					(ack.Byte1 == Byte1) && (ack.Byte2 == Byte2)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	/**
 	 * Ritorna il numero massimo di tentativi di invio da effettuare.
 	 */
 	public int getMaxSendTries() {

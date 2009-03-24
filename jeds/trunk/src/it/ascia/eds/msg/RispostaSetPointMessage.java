f11dd8f14a37ba963c15b4d072e32217f1e7ec02
Sergio Strampelli <sergio.strampelli@gmail.com>
2009-03-24 12:30:17 +0000

    messaggi setPoint

--- jeds/trunk/src/it/ascia/eds/msg/RispostaSetPointMessage.java
+++ jeds/trunk/src/it/ascia/eds/msg/RispostaSetPointMessage.java
@@ -1,24 +1,48 @@
 package it.ascia.eds.msg;
 
-public class RispostaSetPointMessage extends CronotermMessage {
+public class RispostaSetPointMessage extends PTPResponse {
 
 	public RispostaSetPointMessage(int[] message) {
-		super(message);
+		load(message);
 	}
-	
+
 	public int getMessageType() {
 		return EDSMessage.MSG_RISPOSTA_SET_POINT;
 	}
 
 	public String getMessageDescription() {
-		return "Set point cronotermostato";
+		return "Risposta Set point cronotermostato";
 	}
 
-	/**
-	 * Ritorna la temperatura di set point.
-	 */
 	public double getSetPoint() {
-		return (Byte1 & 0x7F) + ((Byte1 & 0x80) >> 7) / 2.0;
+		return ImpostaSetPointMessage.temperatura(Byte1 & 0x3F,(Byte1 & 0x80) >> 7);
 	}
+
+	public int getOra() {
+		return (Byte2 & 0xF8) >> 3;
+	}
+
+	public int getGiorno() {
+		return Byte2 & 0x07;
+	}
+	
+	public int getStagione() {
+		return (Byte1 & 0x40) >> 6;
+	}
+
+	public String toString()	{
+		StringBuffer s = new StringBuffer();
+		s.append(super.toString());
+		if (getOra() == 31) {
+			s.append(" Manuale per "+getGiorno()+" ore");
+		} else {
+			s.append(" Stagione:"+ImpostaSetPointMessage.stagione(getStagione()));
+			s.append(" Giorno:"+ImpostaSetPointMessage.giorno(getGiorno()));
+			s.append(" Orario:"+ImpostaSetPointMessage.fasciaOraria(getOra()));
+		}
+		s.append(" Temperatura: " + getSetPoint()+" gradi C");
+		return s.toString();
+	}
+
 
 }

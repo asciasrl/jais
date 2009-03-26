c07001b9e93ec1ddfab0f30c1fbf26b294fe5d4b
sergio <sergio@bafc8c6a-972d-4a8c-9a31-01add4bfe0c7>
2009-03-26 11:20:22 +0000

    log

--- jeds/trunk/src/it/ascia/eds/EDSControllerModule.java
+++ jeds/trunk/src/it/ascia/eds/EDSControllerModule.java
@@ -61,7 +61,7 @@ public class EDSControllerModule extends BUSControllerModule {
 		}				
  		int autoupdate = config.getInt("autoupdate",0);
  		autoUpdater = new AutoUpdater(autoupdate);
- 		autoUpdater.setName(getClass().getSimpleName()+"-autoUpdater");
+ 		autoUpdater.setName("AutoUpdater");
  		running = true;
  		autoUpdater.start();
  		logger.info("Completato start");

c979ecedcba6501d623ae1271f05b4d7bde85d8e
Sergio Strampelli <sergio.strampelli@gmail.com>
2009-04-01 18:44:05 +0000

    debug: calcolo checksum

--- jdxp/trunk/src/it/ascia/dxp/DXPMessage.java
+++ jdxp/trunk/src/it/ascia/dxp/DXPMessage.java
@@ -59,7 +59,7 @@ public abstract class DXPMessage extends Message {
 		return message; 
 	}
 
-	protected int getShort() {
+	public int getShort() {
 		return (dato1 << 8) + dato0;
 	}
 		
@@ -67,12 +67,46 @@ public abstract class DXPMessage extends Message {
 	 * Calcola il checksum.
 	 */
 	public int checkSum() {
-		return (Start+funzione+tipo+indirizzo+dato1+dato0) & 0xff; 
+		return 0xff - ((Start+funzione+tipo+indirizzo+dato1+dato0) & 0xff); 
 	}
 
 	
 	public String toString() {
-		return indirizzo + " " + getMessageDescription()+" ("+b2h(funzione)+","+b2h(tipo)+") ["+b2b(dato1)+":"+b2b(dato0)+" "+b2h(dato1)+":"+b2h(dato0)+"]";
+		return indirizzo + " " + getMessageDescription()+" (FUN:"+b2h(funzione)+" TYP:"+b2h(tipo)+") ["+b2b(dato1)+":"+b2b(dato0)+" "+b2h(dato1)+":"+b2h(dato0)+"]";
+	}
+
+	/**
+	 * Ritorna il contenuto del messaggio sotto forma di array di interi.
+	 */
+	public int[] getRawMessage() {
+		int message[] = new int[7];
+		message[0] = Start;
+		message[1] = funzione;
+		message[2] = tipo;
+		message[3] = indirizzo;
+		message[4] = dato1;
+		message[5] = dato0;
+		message[6] = checkSum();
+		return message; 
+	}
+
+	/**
+	 * Ritorna il messaggio riportandone i campi in esadecimale.
+	 */
+	public String toHexString()
+	{
+		StringBuffer s = new StringBuffer();
+		if (rawmessage == null) {
+			rawmessage = getRawMessage();
+		}
+		s.append("STX:"+b2h(rawmessage[0])+" ");
+		s.append("FUN:"+b2h(rawmessage[1])+" ");
+		s.append("TYP:"+b2h(rawmessage[2])+" ");
+		s.append("ADD:"+b2h(rawmessage[3])+" ");
+		s.append("DA1:"+b2h(rawmessage[4])+" ");
+		s.append("DA0:"+b2h(rawmessage[5])+" ");
+		s.append("CHK:"+b2h(rawmessage[6])+" ");
+		return s.toString();
 	}
 	
 	public int getMessageType() {

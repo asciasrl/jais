bfcafeeb29996f8ac850e0f638c554c0c9244299
Sergio Strampelli <sergio.strampelli@gmail.com>
2009-04-24 16:30:18 +0000

    debug

--- jeds/trunk/src/it/ascia/eds/EDSControllerModule.java
+++ jeds/trunk/src/it/ascia/eds/EDSControllerModule.java
@@ -4,6 +4,7 @@ import java.util.Iterator;
 import java.util.List;
 
 import org.apache.commons.configuration.HierarchicalConfiguration;
+import org.apache.commons.configuration.SubnodeConfiguration;
 
 import it.ascia.ais.AISException;
 import it.ascia.ais.BUSControllerModule;
@@ -24,16 +25,21 @@ public class EDSControllerModule extends BUSControllerModule {
 		 	try {
 		 		eds = new EDSConnector(sub.getString("name"),controller);
 		 		eds.setAddress(sub.getInt("computer",250));
-		 		// attiva il transport
-		 		String type = sub.getString("transport.type");
+		 		// attiva il transport		 		
+		 		List transports = sub.configurationsAt("transport");
+		 		if (transports.size() == 0) {
+		 			throw(new AISException("Trasport not defined for connector "+eds.getName()));
+		 		}
+	 			SubnodeConfiguration transportConfig = (SubnodeConfiguration) transports.get(0);
+		 		String type = transportConfig.getString("type");
 		 		if (type.equals("serial")) {
-		 			String port = sub.getString("transport.port");
-		 			int speed = sub.getInt("transport.speed");
+		 			String port = transportConfig.getString("port");
+		 			int speed = transportConfig.getInt("speed");
 		 			transport = new SerialTransport(eds,port,speed);
 		 			logger.info("Connesso via seriale a "+port+" a "+speed+"bps");
 		 		} else if (type.equals("tcp")) {
-		 			String host = sub.getString("transport.host");
-		 			int port = sub.getInt("transport.port");					
+		 			String host = transportConfig.getString("host");
+		 			int port = transportConfig.getInt("port");					
 		 			transport = new TCPSerialTransport(eds,host,port);
 		 			logger.info("Connesso via seriale a "+host+":"+port);
 				} else {
@@ -45,14 +51,20 @@ public class EDSControllerModule extends BUSControllerModule {
 			 	List discover = sub.getList("discover",null);
 			 	if (discover == null) {
 			 		logger.debug("Nessun dispositivo da ricercare (discover)");
-			 	}
-				for(Iterator i = discover.iterator(); i.hasNext();) {
-					try {
-						eds.discoverBMC(new Integer((String) i.next()).intValue());						
-					} catch (NumberFormatException e) {
-						logger.error("Indirizzo dispositivo da cercare non corretto: "+e.getMessage());
+			 	} else {
+					for(Iterator i = discover.iterator(); i.hasNext();) {
+						try {
+							eds.discoverBMC(new Integer((String) i.next()).intValue());						
+						} catch (NumberFormatException e) {
+							logger.error("Indirizzo dispositivo da cercare non corretto: "+e.getMessage());
+						}
 					}
-				}
+			 	}
+				// carica il file di configurazione
+			 	String configFileName = sub.getString("config",null);
+			 	if (configFileName != null) {
+			 		eds.loadConfig(configFileName);
+			 	}				
 			 	// registra il connector
 				controller.registerConnector(eds);		
 				myConnectors.add(eds);

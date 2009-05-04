f2cf614d50f2627abb453e8330687f88950eb5d5
Sergio Strampelli <sergio.strampelli@gmail.com>
2009-05-04 20:21:43 +0000

    Metodo toString di RispostaModello: eliminato tentativo di decodifica del tipo che e' ridondante e non allineata

--- jeds/trunk/src/it/ascia/eds/msg/RispostaModelloMessage.java
+++ jeds/trunk/src/it/ascia/eds/msg/RispostaModelloMessage.java
@@ -25,60 +25,8 @@ public class RispostaModelloMessage extends PTPResponse {
 	public String toString()	{
 		StringBuffer s = new StringBuffer();
 		s.append(super.toString()+" ");
-		switch (Byte1) {
-			case 8:			
-				s.append("BMC Standard I/O, 0 IN - 8 OUT");
-				break;
-			case 40:			
-				s.append("BMC Standard I/O, 4 IN - 0 OUT");
-				break;
-			case 44:			
-				s.append("BMC Standard I/O, 4 IN - 4 OUT");
-				break;
-			case 60:			
-				s.append("BMC Standard I/O, 6 IN - 0 OUT");
-				break;
-			case 88:			
-				s.append("BMC Standard I/O, 8 IN - 8 OUT");
-				break;
-			case 102:			
-				s.append("DIMMER Evolution low power (800W, 2 canali)");
-				break;
-			case 106:			
-				s.append("DIMMER 0-10 V con 1 out SPERIMENTALE ?");
-				break;
-			case 111:			
-				s.append("Dimmer 0-10 V con 1 out");
-				break;
-			case 121:			
-				s.append("Sonda di temperatura");
-				break;
-			case 122:			
-				s.append("Regolatore di temperatura");
-				break;
-			case 127:			
-				s.append("Cronotermostato");
-				break;
-			case 131:			
-				s.append("INT-IR Receiver");
-				break;
-			case 152:			
-				s.append("Centralina scenari 2 ingressi");
-				break;
-			case 154:			
-				s.append("Centralina scenari 4 ingressi");
-				break;
-			case 156:			
-				s.append("Centralina scenari 6 ingressi");
-				break;
-			case 158:			
-				s.append("Centralina scenari 8 ingressi");
-				break;
-			default:
-				s.append("sconosciuto");
-				break;
-		}
-		s.append(" Revisione:"+Byte2);
+		s.append(" Model:"+Byte1);
+		s.append(" Revision:"+Byte2);
 		return s.toString();
 	}
 

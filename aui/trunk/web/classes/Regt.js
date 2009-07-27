05335739e893f36f0336c48a5c6615b974d52a79
Sergio Strampelli <sergio.strampelli@gmail.com>
2009-07-27 08:18:55 +0000

    regt (abbozzo)

--- aui/trunk/web/classes/Regt.js
+++ aui/trunk/web/classes/Regt.js
@@ -3,12 +3,11 @@ if (AUI.Regt == undefined) {
 	AUI.Regt = {
 
 		init : function() {
+			this.factor = 2;
 			this.request = AUI.Http.getRequest();
-			AUI.Regt.get('str.70');
-		},
-		
-		stateChange : function() {
-			// todo
+			AUI.Regt.get('1.255');
+			AUI.Logger.setLevel(2);
+			var self = this;
 		},
 		
 		/**
@@ -32,6 +31,7 @@ if (AUI.Regt == undefined) {
 						AUI.Header.show(request.responseText);
 					} else {
 						AUI.Logger.info("request.status:"+request.status);
+						//AUI.Logger.debug("Response:"+request.responseText);
 						var data;
 						eval("data="+request.responseText+";");
 						AUI.Regt.update(data);
@@ -53,9 +53,10 @@ if (AUI.Regt == undefined) {
 			for (var stagione = 0; stagione <= 1; stagione++) {
 				for (var giorno = 0; giorno <= 6; giorno++) {
 					for (var ora = 0; ora <= 23; ora++) {
-						// setPoint-Inverno-Dom-02
+						// setPoint-1-2-3
 						var id = stagione + "-" + giorno+"-"+ora;
 						var value = data[0].Status["setPoint-"+id].V;
+						AUI.Logger.debug("id="+id+" value="+value);
 						AUI.Regt.updateBar(id,value);
 					};
 				};
@@ -63,34 +64,42 @@ if (AUI.Regt == undefined) {
 		},
 		
 		updateBar : function(id,value) {
-			var el = document.getElementById("eds-regt-" + id + "-bar");
+			var el = document.getElementById("eds-regt-" + id);
 			if (value == null) {
 				value = 0;
 			};
-			el.style.height = (value + 15) + 'px';
-			//el.innerHTML = value;
+			el.style.bottom = (value * this.factor) + 'px';
+			el.innerHTML = value;
 		},
 		
 		onMouseOver : function(event,id) {
-				//event.preventDefault();
-				//event.stopPropagation();
-				AUI.Logger.log("x:"+event.clientX+" y:"+event.clientY);
-				var el = document.getElementById("regt-" + id);
-				// TODO calcolare offset
-				//alert(el.offsetTop);
-				//var value = event.clientY - el.top;
-				var value = el.offsetTop + 180 - event.clientY;
-				AUI.Regt.updateBar(id,value);
-				var self = this;
-				self.id = id;
-				this.mousemove = function(e) { return self.onMouseMove(e) };
-				el.addEventListener('mousemove', this.mousemove , false);
+			AUI.Logger.log("onMouseOver id="+id+" x:"+event.clientX+" y:"+event.clientY);
+			//event.preventDefault();
+			//event.stopPropagation();
+			var el = document.getElementById("eds-regt-" + id);
+			// TODO calcolare offset
+			//alert(el.offsetTop);
+			//var value = event.clientY - el.top;
+			this.el = el;
+			var parent = el.parentNode;
+			this.parent = parent;
+			var position = AUI.Regt.getPosition(parent);
+			this.y = position.y + parent.offsetHeight; 
+			//AUI.Regt.updateBar(id,value);
+			var self = this;
+			self.id = id;
+			this.mousemove = function(e) { return self.onMouseMove(e,id); };
+			el.addEventListener('mousemove', this.mousemove , false);
+			AUI.Logger.log("onMouseOver y:"+this.y);
 		},
 		
 		onMouseMove : function(event,id) {
-			var el = document.getElementById("regt-" + this.id);
-			var value = el.offsetTop + 180 - event.clientY;
+			var el = document.getElementById("eds-regt-" + this.id);
+			var value = (this.y - event.clientY - window.scrollY) / this.factor;
+			AUI.Logger.log("onMouseMove id="+id+" x:"+event.clientX+" y:"+event.clientY+" value="+value);
+			//var value = el.offsetTop + 180 - event.clientY;
 			AUI.Regt.updateBar(id,value);
+			return false;
 		},
 		
 		onMouseOut : function(event,id) {
@@ -104,7 +113,24 @@ if (AUI.Regt == undefined) {
 		
 		timeoutExpired : function() {
 			AUI.Regt.abort();
+		},
+		
+		getPosition : function(e){
+			var left = 0;
+			var top  = 0;
+
+			while (e.offsetParent){
+				left += e.offsetLeft;
+				top  += e.offsetTop;
+				e     = e.offsetParent;
+			}
+
+			left += e.offsetLeft;
+			top  += e.offsetTop;
+			
+			return {x:left, y:top};
 		}
+
 
 	};
 }		

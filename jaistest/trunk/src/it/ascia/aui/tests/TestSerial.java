0a0af79410ce6f877350513e128c660545bc0d26
sergio <sergio@bafc8c6a-972d-4a8c-9a31-01add4bfe0c7>
2009-09-15 10:25:25 +0000

    test

--- jaistest/trunk/src/it/ascia/aui/tests/TestSerial.java
+++ jaistest/trunk/src/it/ascia/aui/tests/TestSerial.java
@@ -1,10 +1,9 @@
 package it.ascia.aui.tests;
 
-import java.util.Random;
-
 import org.apache.log4j.Logger;
 
 import it.ascia.ais.Controller;
+import it.ascia.ais.SerialTransport;
 import it.ascia.eds.EDSConnector;
 import it.ascia.eds.msg.EDSMessage;
 import it.ascia.eds.msg.RichiestaModelloMessage;
@@ -14,27 +13,34 @@ import junit.framework.TestSuite;
 
 public class TestSerial extends TestCase {
 	
+	private static final int DEST = 3;
+
 	public static Test suite() {
 		TestSuite ts = new TestSuite();
 		ts.addTestSuite(TestSerial.class);
 		return new AuiTestSetup(ts);
 	}
 
-	public int RichiestaModelloMessage_Bus1_a255(int n) {
+	public int RichiestaModelloMessage_Bus1_aDEST(int n) {
 		Logger log = Logger.getLogger(getClass());
 		Controller c = Controller.getController();
-		EDSConnector eds1 = (EDSConnector) c.getConnector("1");  
+		EDSConnector eds = (EDSConnector) c.getConnector("6");  
 		EDSMessage msg;
 		int success = 0;
 		int slows = 0;
 		int fails = 0;
+		long start0 = System.nanoTime();
 		long start;
+		long speed = 0;
 		//n = n * 10;
+		//SerialTransport tr = (SerialTransport) eds.getTransport();
+		//byte[] b = {0x55};
 		for (int i=1; i <= n; i++) {
 			//log.info("Messaggio di test n. "+i);
-			msg = new RichiestaModelloMessage(255,1);
+			msg = new RichiestaModelloMessage(DEST,76);
 			start = System.nanoTime();
-			if (eds1.sendMessage(msg)) {
+			//tr.write(b);
+			if (eds.sendMessage(msg)) {
 				success++;
 			} else {
 				fails++;
@@ -47,50 +53,59 @@ public class TestSerial extends TestCase {
 			} else {
 				log.debug(s);
 			}
+			dt = ((System.nanoTime()-start0)/1000000000L);
+			if (dt > 0 && (i %10)==0 ) {
+				speed = i / dt;
+				System.out.print(speed + " "+i+"/"+success+"/"+slows+"/"+ fails+"\r");
+			}
 		}
 		log.info("Successi="+success+" Lenti="+slows+" Fallimenti="+fails);
 		//return success/10;
 		return success;
 	}
 
-	public void test00_1_RichiestaModelloMessage_Bus1_a255() {
-    	assertEquals(1, RichiestaModelloMessage_Bus1_a255(1));				
+	public void test00_1_RichiestaModelloMessage_Bus1_aDEST() {
+    	assertEquals(1, RichiestaModelloMessage_Bus1_aDEST(1));				
 	}
 
-	public void test01_100_RichiestaModelloMessage_Bus1_a255() {
-    	assertEquals(100, RichiestaModelloMessage_Bus1_a255(100));				
+	public void test01_100_RichiestaModelloMessage_Bus1_aDEST() {
+    	assertEquals(100, RichiestaModelloMessage_Bus1_aDEST(100));				
 	}
 
-	public void test02_1000_RichiestaModelloMessage_Bus1_a255() {
-    	assertEquals(1000, RichiestaModelloMessage_Bus1_a255(1000));				
+	public void test02_1000_RichiestaModelloMessage_Bus1_aDEST() {
+    	assertEquals(1000, RichiestaModelloMessage_Bus1_aDEST(1000));				
 	}
 
-	public void test03_1000_RichiestaModelloMessage_Bus1_a255() {
-    	assertEquals(1000, RichiestaModelloMessage_Bus1_a255(1000));				
+	public void test03_1000_RichiestaModelloMessage_Bus1_aDEST() {
+    	assertEquals(1000, RichiestaModelloMessage_Bus1_aDEST(1000));				
 	}
 
-	public void test04_1000_RichiestaModelloMessage_Bus1_a255() {
-    	assertEquals(1000, RichiestaModelloMessage_Bus1_a255(1000));				
+	public void test04_1000_RichiestaModelloMessage_Bus1_aDEST() {
+    	assertEquals(1000, RichiestaModelloMessage_Bus1_aDEST(1000));				
 	}
 
-	public void test05_1000_RichiestaModelloMessage_Bus1_a255() {
-    	assertEquals(1000, RichiestaModelloMessage_Bus1_a255(1000));				
+	public void test05_1000_RichiestaModelloMessage_Bus1_aDEST() {
+    	assertEquals(1000, RichiestaModelloMessage_Bus1_aDEST(1000));				
 	}
 
-	public void test06_1000_RichiestaModelloMessage_Bus1_a255() {
-    	assertEquals(1000, RichiestaModelloMessage_Bus1_a255(1000));				
+	public void test06_1000_RichiestaModelloMessage_Bus1_aDEST() {
+    	assertEquals(1000, RichiestaModelloMessage_Bus1_aDEST(1000));				
 	}
 
-	public void test07_1000_RichiestaModelloMessage_Bus1_a255() {
-    	assertEquals(1000, RichiestaModelloMessage_Bus1_a255(1000));				
+	public void test07_1000_RichiestaModelloMessage_Bus1_aDEST() {
+    	assertEquals(1000, RichiestaModelloMessage_Bus1_aDEST(1000));				
 	}
 
-	public void test08_1000_RichiestaModelloMessage_Bus1_a255() {
-    	assertEquals(1000, RichiestaModelloMessage_Bus1_a255(1000));				
+	public void test08_1000_RichiestaModelloMessage_Bus1_aDEST() {
+    	assertEquals(1000, RichiestaModelloMessage_Bus1_aDEST(1000));				
 	}
 
-	public void test09_1000_RichiestaModelloMessage_Bus1_a255() {
-    	assertEquals(1000, RichiestaModelloMessage_Bus1_a255(1000));				
+	public void test09_1000_RichiestaModelloMessage_Bus1_aDEST() {
+    	assertEquals(1000, RichiestaModelloMessage_Bus1_aDEST(1000));				
+	}
+
+	public void test10_10K_RichiestaModelloMessage_Bus1_aDEST() {
+    	assertEquals(10000, RichiestaModelloMessage_Bus1_aDEST(10000));				
 	}
 
 	/*
@@ -100,9 +115,9 @@ public class TestSerial extends TestCase {
 		EDSConnector eds1 = (EDSConnector) c.getConnector("1");  
 		EDSConnector eds2 = (EDSConnector) c.getConnector("2");
 		EDSMessage msg;
-		for (int i=3; i < 255; i++) {
-			//int a = r.nextInt(255);
-			//int b = r.nextInt(255);			
+		for (int i=3; i < DEST; i++) {
+			//int a = r.nextInt(DEST);
+			//int b = r.nextInt(DEST);			
 			msg = new RichiestaModelloMessage(i,1);
 			eds1.sendMessage(msg);
 			msg = new RichiestaModelloMessage(i,2);

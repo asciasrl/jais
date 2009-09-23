f9fac5cc9f24b90f7959f97c4289978531179d28
Sergio Strampelli <sergio.strampelli@gmail.com>
2009-09-23 18:07:07 +0000

    nuova porta ScenePort

---
+++ jais/trunk/src/it/ascia/ais/ScenePort.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package it.ascia.ais;
+
+/**
+ * Questa porta ha sempre valore false. Viene usata solo per attivare scenari 
+ * 
+ * @author Sergio
+ *
+ */
+public class ScenePort extends TriggerPort {
+
+	public ScenePort(Device device, String portId) {
+		super(device, portId);
+	}
+	
+}

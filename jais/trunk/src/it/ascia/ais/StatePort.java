c7078e987ae3a7cbbf85e758c461fa350dd0b216
sergio <sergio@bafc8c6a-972d-4a8c-9a31-01add4bfe0c7>
2009-10-21 21:54:50 +0000

    statePort: obbligo dei tags

--- jais/trunk/src/it/ascia/ais/StatePort.java
+++ jais/trunk/src/it/ascia/ais/StatePort.java
@@ -2,15 +2,6 @@ package it.ascia.ais;
 
 public class StatePort extends DevicePort {
 
-	public StatePort(Device device, String portId) {
-		super(device, portId, null);
-	}
-
-	public StatePort(Device device, String portId, String portName) {
-		super(device, portId, portName);
-		// TODO Auto-generated constructor stub
-	}
-
 	public StatePort(Device device, String portId, String portName, String[] tags) {
 		super(device, portId, portName);
 		setTags(tags);

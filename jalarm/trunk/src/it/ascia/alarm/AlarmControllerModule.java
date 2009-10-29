f31420d805473848d03d19599f6445b381dc84dc
sergio <sergio@bafc8c6a-972d-4a8c-9a31-01add4bfe0c7>
2009-10-29 08:28:40 +0000

    debug + RPC

--- jalarm/trunk/src/it/ascia/alarm/AlarmControllerModule.java
+++ jalarm/trunk/src/it/ascia/alarm/AlarmControllerModule.java
@@ -3,6 +3,8 @@ package it.ascia.alarm;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
+import org.jabsorb.JSONRPCBridge;
+
 import it.ascia.ais.ControllerModule;
 import it.ascia.ais.DevicePort;
 import it.ascia.ais.DevicePortChangeEvent;
@@ -18,29 +20,49 @@ public class AlarmControllerModule extends ControllerModule implements PropertyC
 	private String alarmPortId;
 	private DevicePort alarmPort;
 
-	// tempo di uscita
-	private long exitDelay = 30000;
+	// tempo di uscita (mS)
+	private long exitDelay;
 	private long exittime = 0;
 	
-	// tempo di entrata 	
-	private long enterDelay = 30000;
+	// tempo di entrata (mS)
+	private long enterDelay;
 
-	// tempo di allarme
-	private long alarmDuration = 120000;
+	// tempo di allarme (mS)
+	private long alarmDuration;
 
 	private Thread alarmThread;
 	
 	public void start() {
-		// FIXME leggere porte dal file di configurazione 
-		preAlarmPort = controller.getDevicePort(alarmArmedPortId);
-		preAlarmPort.addPropertyChangeListener(this);
+		super.start();
+		
+		alarmArmedPortId = getConfiguration().getString("alarmArmedPortId",null);
 		alarmArmedPort = controller.getDevicePort(alarmArmedPortId);
 		alarmArmedPort.addPropertyChangeListener(this);
+		logger.info("Alarm armed port=" + alarmArmedPortId);
+
+		preAlarmPortId = getConfiguration().getString("preAlarmPortId",null);;
+		preAlarmPort = controller.getDevicePort(preAlarmPortId);
+		preAlarmPort.addPropertyChangeListener(this);
+		exitDelay = getConfiguration().getLong("exitDelay",30);
+		enterDelay = getConfiguration().getLong("enterDelay",30);
+		logger.info("Prealarm port=" + preAlarmPortId + " exit=" + exitDelay + "s enter=" + enterDelay + "s");
+		exitDelay *= 1000;
+		enterDelay *= 1000;
+		
+		alarmPortId = getConfiguration().getString("alarmPortId",null);;
 		alarmPort = controller.getDevicePort(alarmPortId);
-		super.start();
+		alarmDuration = getConfiguration().getLong("alarmDuration",120);		
+		logger.info("Alarm port=" + alarmPortId + " duration=" + alarmDuration + "s");
+		alarmDuration *= 1000;
+
+		JSONRPCBridge.getGlobalBridge().registerObject("Alarm", new AlarmRPCServer(this));
 	}
 	
-	private boolean isAlarmActive() {
+	/**
+	 * 
+	 * @return value of alarmArmedPort
+	 */
+	public boolean isArmed() {
 		return ((Boolean) (alarmArmedPort.getValue())).booleanValue();
 	}
 	
@@ -48,22 +70,34 @@ public class AlarmControllerModule extends ControllerModule implements PropertyC
 		if (DevicePortChangeEvent.class.isInstance(evt)) {
 			String address = ((DevicePortChangeEvent)evt).getFullAddress();
 			Object value = ((DevicePortChangeEvent)evt).getNewValue();
-			if (address.equals(preAlarmPortId) && 
-					((Boolean)value).booleanValue() && 
-					isAlarmActive()) {
-				if (exittime > 0 && 
+			if (value == null) {
+				logger.warn("Port "+address+", value null");
+				return;
+			}
+			if (!Boolean.class.isInstance(value)) {
+				logger.error("Port "+address+" must be Boolean, not "+value.getClass().getSimpleName());
+			}
+			boolean bValue = ((Boolean)value).booleanValue(); 
+			logger.debug("Ricevuto: "+address+"="+value);
+			if (address.equals(preAlarmPortId) && bValue) {
+				if (!isArmed()) {
+					logger.trace("Alarm disabled, ignore prealarms.");
+				} else if (exittime > 0 && 
 						System.currentTimeMillis() < (exittime + exitDelay)) {
 					logger.debug("In exit time, ignore prealarms.");
 				} else if (alarmThread != null && alarmThread.isAlive()) {
 					logger.debug("Already in alarm");
 				} else {
 					alarmThread = new AlarmThread();
+					alarmThread.setName("Alarm "+alarmPortId);
 					alarmThread.start();
 				}					 
-			} else if (address.equals(alarmPortId)) {
-				if (((Boolean)value).booleanValue()) {
+			} else if (address.equals(alarmArmedPortId)) {
+				if (bValue) {
+					logger.info("Alarm armed");
 					exittime = System.currentTimeMillis();
 				} else {
+					logger.info("Alarm disarmed");
 					if (alarmThread != null && alarmThread.isAlive()) {
 						alarmThread.interrupt();
 						try {
@@ -83,14 +117,51 @@ public class AlarmControllerModule extends ControllerModule implements PropertyC
 			try {
 				logger.info("Start entering delay "+enterDelay/1000+"sec");
 				Thread.sleep(enterDelay);
-				alarmPort.writeValue(new Boolean(true));
-				logger.info("Start alarm for "+alarmDuration/1000+"sec");
-				Thread.sleep(alarmDuration);
+				try {
+					alarmPort.writeValue(new Boolean(true));
+					logger.info("Start alarm for "+alarmDuration/1000+"sec");
+					Thread.sleep(alarmDuration);
+				} catch (InterruptedException e) {
+					logger.info("Alarm interrupted during alarm");
+				}
+				alarmPort.writeValue(new Boolean(false));
 			} catch (InterruptedException e) {
-				logger.info("Interrupted alarm");
+				logger.info("Alarm interrupted during enter delay");
 			}
-			alarmPort.writeValue(new Boolean(false));
-			logger.info("End alarme");
+			logger.info("End alarm cycle");
+		}
+	}
+
+	public boolean checkPin(String pin) {
+		String pin1 = getConfiguration().getString("PIN",null);
+		return pin.equals(pin1);
+	}
+	
+	public boolean arm(String pin) {
+		if (checkPin(pin)) {
+			if (alarmArmedPort.writeValue(new Boolean(true))) {
+				logger.info("Alarm armed with PIN");
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean disarm(String pin) {
+		if (checkPin(pin)) {
+			if (alarmArmedPort.writeValue(new Boolean(false))) {
+				logger.info("Alarm disarmed with PIN");
+				return true;
+			}
+		}		
+		return false;
+	}
+
+	public boolean toggle(String pin) {
+		if (isArmed()) {
+			return disarm(pin);
+		} else {
+			return arm(pin);
 		}
 	}
 	
---
+++ jalarm/trunk/src/it/ascia/alarm/AlarmRPCServer.java
@@ -0,0 +1,61 @@
+/**
+ * (C) 2009 Ascia S.r.l.
+ */
+package it.ascia.alarm;
+
+import java.io.Serializable;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Server RPC, espone i metodi usati dal client RPC
+ * @author Sergio
+ *
+ */
+public class AlarmRPCServer implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8944342209414623443L;
+
+	private static final String ATTRIBUTE_PIN = "Alarm.pin";
+
+    protected Logger logger;
+    
+	private AlarmControllerModule module; 
+	
+	public AlarmRPCServer(AlarmControllerModule controllerModule ) {
+		logger = Logger.getLogger(getClass());		
+		module = controllerModule;
+	}
+	
+	public boolean pin(HttpSession session, String pin) {
+		if (module.checkPin(pin)) {
+			session.setAttribute(ATTRIBUTE_PIN,pin);
+			return true;
+		} else {
+			session.removeAttribute(ATTRIBUTE_PIN);
+			return false;
+		}
+	}
+	
+	public boolean isArmed(HttpSession session) {
+		return module.isArmed();
+	}
+
+	public boolean toggle(HttpSession session) {
+		return module.toggle((String) session.getAttribute(ATTRIBUTE_PIN));
+	}
+	
+	public boolean arm(HttpSession session) {
+		return module.arm((String) session.getAttribute(ATTRIBUTE_PIN));
+	}	
+
+	public boolean disarm(HttpSession session) {
+		return module.disarm((String) session.getAttribute(ATTRIBUTE_PIN));
+	}	
+
+}
